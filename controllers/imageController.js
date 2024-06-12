06121eeab073b119be56f1c4c8aa9514b800217d
pradiptechie <kumarpradip3956@gmail.com>
2024-06-13 01:35:48 +0545

    problem with permission

--- controllers/imageController.js
+++ controllers/imageController.js
@@ -6,18 +6,19 @@ const uploadImage = async (req, res) => {
   try {
     const { image } = req.body;
     const originalname = image.originalname;
-
-    //yeha samw image data aaudaix, aba image lai properly handle grnu x, there may be problem below here..
-    const buffer = Buffer.from(image, 'base64'); // Error occurs here
+    
+    // Use the buffer property directly
+    const buffer = image.buffer;
     console.log(buffer);
     const filename = `${Date.now()}-${originalname}`;
     const originalPath = path.join(__dirname, "../../images/original/", filename);
     const optimizedPath = path.join(__dirname, "../../images/optimized/", filename);
 
-    await fs.promises.writeFile(originalPath, buffer);
+    await fs.promises.writeFile(originalPath, Buffer.from(buffer));
+
     await sharp(originalPath).resize(800).toFile(optimizedPath);
 
-    fs.unlinkSync(originalPath); // Delete original image after optimization
+    fs.unlinkSync(originalPath); // problem here, permission related
 
     const imageUrl = `/images/optimized/${filename}`;
     res.status(200).json({ imageUrl });
@@ -26,6 +27,7 @@ const uploadImage = async (req, res) => {
     res.status(500).json({ error: "Error processing image" });
   }
 };
+
 
 
 const getImage = (req, res) => {
