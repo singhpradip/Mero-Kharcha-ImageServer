59671e0f9ee675ae6915a23687bf2839bfa7ce88
pradiptechie <kumarpradip3956@gmail.com>
2024-06-15 00:43:10 +0545

    upload and get image file is done

--- controllers/imageController.js
+++ controllers/imageController.js
@@ -1,26 +1,35 @@
 const path = require("path");
 const sharp = require("sharp");
 const fs = require("fs");
+require("dotenv").config();
 
 const uploadImage = async (req, res) => {
   try {
     const { image } = req.body;
     const originalname = image.originalname;
-    
+
     // Use the buffer property directly
     const buffer = image.buffer;
-    console.log(buffer);
+    // console.log(buffer);
     const filename = `${Date.now()}-${originalname}`;
-    const originalPath = path.join(__dirname, "../../images/original/", filename);
-    const optimizedPath = path.join(__dirname, "../../images/optimized/", filename);
+    const originalPath = path.join(
+      __dirname,
+      "../../images/original/",
+      filename
+    );
+    const optimizedPath = path.join(
+      __dirname,
+      "../../images/optimized/",
+      filename
+    );
 
     await fs.promises.writeFile(originalPath, Buffer.from(buffer));
 
     await sharp(originalPath).resize(800).toFile(optimizedPath);
 
-    fs.unlinkSync(originalPath); // problem here, permission related
+    // fs.unlinkSync(originalPath); // problem here, permission related
 
-    const imageUrl = `/images/optimized/${filename}`;
+    const imageUrl = `http://localhost:${process.env.PORT}/${filename}`;
     res.status(200).json({ imageUrl });
   } catch (error) {
     console.log(error);
@@ -28,15 +37,18 @@ const uploadImage = async (req, res) => {
   }
 };
 
-
-
 const getImage = (req, res) => {
   const filePath = path.join(
     __dirname,
     "../../images/optimized/",
     req.params.filename
   );
-  res.sendFile(filePath);
+  // console.log(filePath);
+  res.sendFile(filePath, (err) => {
+    if (err) {
+      res.status(404).json({ error: "Image not found" });
+    }
+  });
 };
 
 const deleteImage = (req, res) => {
